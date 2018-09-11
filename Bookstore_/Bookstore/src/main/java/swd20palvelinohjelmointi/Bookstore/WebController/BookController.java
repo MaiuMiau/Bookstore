07f3aae3a87d51e91be2e5d504f1bc89e9b1f36c
unknown <maiju.nurmi@myy.haaga-helia.fi>
2018-09-11 15:56:21 +0300

    osa3 tehtävien 2 ja 3 tekoa

--- Bookstore_/Bookstore/src/main/java/swd20palvelinohjelmointi/Bookstore/WebController/BookController.java
+++ Bookstore_/Bookstore/src/main/java/swd20palvelinohjelmointi/Bookstore/WebController/BookController.java
@@ -1,18 +1,47 @@
 package swd20palvelinohjelmointi.Bookstore.WebController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import swd20palvelinohjelmointi.Bookstore.Domain.Book;
+import swd20palvelinohjelmointi.Bookstore.Domain.BookRepository;
 
 @Controller
 public class BookController {
 	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String Books() {
-		
-		return "";
-	}
-
+	@Autowired
+	private BookRepository repository;
+	
+	 @RequestMapping(value="/booklist")
+	    public String bookList(Model model) {	
+	        model.addAttribute("books", repository.findAll());// modelin kautta palauttaa listan kirjoista
+	        return "booklist";
+	    }
+	 
+	 @RequestMapping(value = "/add")
+	    public String addBook(Model model){
+	    	model.addAttribute("book", new Book());
+	        return "addbook";
+	    }    
+	 
+	 @RequestMapping(value = "/save", method = RequestMethod.POST)
+	    public String save(Book book){
+	        repository.save(book);
+	        return "redirect:booklist"; // ettei tulisi selaimen virkistyksen jälkeen tahttomia uudelleen postauksia
+	    }  
+	 
+	 @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+	    	repository.deleteById(bookId);
+	        return "redirect:../booklist"; // .. tarkoittaa ett siirrytään urlissa
+	    }    
+	
+	
 	
 	//git kokeilu kommentti
 }
